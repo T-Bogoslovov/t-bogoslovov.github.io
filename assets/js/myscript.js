a41d6a74a01f9bd6ea78a9d64b9e12a12768acf5
Tihomir Bogoslovov <60014307+wYh0iUt@users.noreply.github.com>
2020-07-28 09:52:38 +0100

    Script update

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -3,10 +3,9 @@ $(window).scroll(function(e) {
 
     // add/remove class to navbar when scrolling to hide/show
     var scroll = $(window).scrollTop();
-    if (scroll >= 1000) {
+    if (scroll >= 200) {
         $('.navbar').addClass("navbar-hide");
     } else {
         $('.navbar').removeClass("navbar-hide");
     }
-
 });
